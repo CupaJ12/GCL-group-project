771bc3634a08c81682649153ba0ec56aaa4da486
Bryan Highhill <bryanhighhill@gmail.com>
2023-03-30 12:45:07 -0500

    used regex and Number() to remove all non integers and special characters aside from the decimal, from the currency values. created variable gross booking amount which is successfully adding up the values

--- src/components/BookingForm/BookingForm.jsx
+++ src/components/BookingForm/BookingForm.jsx
@@ -26,19 +26,25 @@ const BookingForm = () => {
     const [email, setEmail] = useState('');
     const [checkIn, setCheckIn] = useState(null);
     const [checkOut, setCheckOut] = useState(null);
-    const [costPerNight, setCostPerNight] = useState(0);
+    const [costPerNight, setCostPerNight] = useState('');
+    const [petFees, setPetFees] = useState('');
+    const [cleaningFees, setCleaningFees] = useState('');
+    const [lodgingTax, setLodgingTax] = useState('');
+
+    // Number(costPerNight.replace(/[^0-9.]/g, ''));  - - - removes all special characters EXCEPT for numbers and decimal and converts to an integer
+    const grossBookingAmount = (Number(costPerNight.replace(/[^0-9.]/g, ''))) + (Number(petFees.replace(/[^0-9.]/g, ''))) + (Number(cleaningFees.replace(/[^0-9.]/g, ''))) + (Number(lodgingTax.replace(/[^0-9.]/g, '')));
+
 
     const addAProperty = () => {
         return (
         console.log('you clicked add a property with checkin: ', checkIn)
-        
         )
     };
 
     const onSubmit = () => {
         return (
             console.log('you clicked submit')
-        );
+        )
     };
 
     return (
@@ -147,7 +153,7 @@ const BookingForm = () => {
                             <label className="label" htmlFor="cost-per-night">Cost Per Night</label>
 
                             <CurrencyInput
-                                key="currency-input"
+                                key="currency-input-cost-per-night"
                                 placeholder="$0.00" 
                                 type="text"
                                 id="cost-per-night"
@@ -157,49 +163,58 @@ const BookingForm = () => {
                                 className="financial-input"
                                 onChange={(event) => setCostPerNight(event.target.value)}
                             />
-                            
+
                         </div>
 
-                        <div className="tenant-input-div">
-                            <label className="label" htmlFor="last-name">Last Name</label>
-                            <input
-                                id="last-name"
-                                name="last-name"
+                        <div className="financial-input-div">
+                            <label className="label" htmlFor="pet-fees">Pet Fees</label>
+
+                            <CurrencyInput
+                                key="currency-input-pet-fees"
+                                placeholder="$0.00 (optional)" 
                                 type="text"
-                                value={lastName}
-                                placeholder="last name"
-                                required
-                                className="tenant-input"
-                                onChange={(event) => setLastName(event.target.value)}
+                                id="pet-fees"
+                                name="pet-fees"
+                                value={petFees}
+                                className="financial-input"
+                                onChange={(event) => setPetFees(event.target.value)}
                             />
                         </div>
 
-                        <div className="tenant-input-div">
-                            <label className="label" htmlFor="phone">Phone</label>
+                        <div className="financial-input-div">
+                            <label className="label" htmlFor="cleaning-fees">Cleaning Fees</label>
                             
-                            <input
-                                id="phone"
-                                name="phone"
-                                value={phone}
-                                placeholder="phone (optional)"
-                                className="tenant-input"
-                                onChange={(event) => setPhone(event.target.value)}
+                            <CurrencyInput
+                                key="currency-input-cleaning-fees"
+                                placeholder="$0.00" 
+                                type="text"
+                                id="cleaning-fees"
+                                name="cleaning-fees"
+                                value={cleaningFees}
+                                required
+                                className="financial-input"
+                                onChange={(event) => setCleaningFees(event.target.value)}
                             />
                         </div>
 
-                        <div className="tenant-input-div">
-                            <label className="label" htmlFor="email">Email</label>
-                            <input
-                                id="email"
-                                name="email"
-                                type="email"
-                                value={email}
-                                placeholder="email (optional)"
-                                className="tenant-input"
-                                onChange={(event) => setEmail(event.target.value)}
+                        <div className="financial-input-div">
+                            <label className="label" htmlFor="lodging-tax">Lodging Tax</label>
+
+                            <CurrencyInput
+                                key="currency-input-lodging-tax"
+                                placeholder="$0.00" 
+                                type="text"
+                                id="lodging-tax"
+                                name="lodging-tax"
+                                value={lodgingTax}
+                                required
+                                className="financial-input"
+                                onChange={(event) => setLodgingTax(event.target.value)}
                             />
                         </div>
-                    </div> {/* end of tenant-container */}
+
+                        <h1>Gross Booking Amount</h1>
+                    </div> {/* end of financial-container */}
 
 
                 </form>
