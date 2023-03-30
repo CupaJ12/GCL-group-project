32c493091cc96bc6650eaea3da195c741612959d
Bryan Highhill <bryanhighhill@gmail.com>
2023-03-30 11:56:13 -0500

    created currency input component located in Booking Form folder. Importing and consuming in the Booking Form.jsx and now the currency input mask is working correctly

--- src/components/BookingForm/BookingForm.jsx
+++ src/components/BookingForm/BookingForm.jsx
@@ -6,11 +6,18 @@ import { Dayjs } from 'dayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DateField } from '@mui/x-date-pickers/DateField';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import MaskedInput from 'react-text-mask';
+import CurrencyInput from './CurrencyInput';
+import createNumberMask from 'text-mask-addons/dist/createNumberMask';
+import PropTypes from 'prop-types';
 import './BookingForm.css';
 
 // npm install @mui/x-date-pickers
 // npm install @mui/material @emotion/react @emotion/styled
 // npm install dayjs --save
+// npm i react-text-mask --save
+//npm i text-mask-addons --save
+// npm i prop-types
 
 const BookingForm = () => {
     const [firstName, setFirstName] = useState('');
@@ -19,6 +26,7 @@ const BookingForm = () => {
     const [email, setEmail] = useState('');
     const [checkIn, setCheckIn] = useState(null);
     const [checkOut, setCheckOut] = useState(null);
+    const [costPerNight, setCostPerNight] = useState(0);
 
     const addAProperty = () => {
         return (
@@ -27,7 +35,6 @@ const BookingForm = () => {
         )
     };
 
-
     const onSubmit = () => {
         return (
             console.log('you clicked submit')
@@ -35,7 +42,6 @@ const BookingForm = () => {
     };
 
     return (
-        
             <div className="booking-form-container">
                 
                 <div className="property-select-container">
@@ -49,7 +55,7 @@ const BookingForm = () => {
                     Tenant
                 </div>
 
-                <form onSubmit={onSubmit}>
+                <form key="booking-form" onSubmit={onSubmit}>
 
                     <div className="tenant-container">
                         <div className="tenant-input-div">
@@ -82,7 +88,9 @@ const BookingForm = () => {
 
                         <div className="tenant-input-div">
                             <label className="label" htmlFor="phone">Phone</label>
-                            <input
+                            
+                            <MaskedInput
+                                mask={['(', /[1-9]/, /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/]}
                                 id="phone"
                                 name="phone"
                                 value={phone}
@@ -114,7 +122,6 @@ const BookingForm = () => {
                                     value={checkIn}
                                     onChange={(newValue) => setCheckIn(newValue)}
                                     showDaysOutsideCurrentMonth
-                                    // onChange={(event) => setCheckIn(event.target.value)}
                                 />
                             </LocalizationProvider>
                         </div>
@@ -126,15 +133,73 @@ const BookingForm = () => {
                                     value={checkOut}
                                     onChange={(newValue) => setCheckOut(newValue)}
                                     showDaysOutsideCurrentMonth
-                                    // onChange={(event) => setCheckIn(event.target.value)}
                                 />
                             </LocalizationProvider>
                         </div>
-                    </div>
+                    </div> {/* end of tenant-input-date-div */}
 
                     <div className="section-header">
                         Financial
                     </div>
+
+                    <div className="financial-container">
+                        <div className="financial-input-div">
+                            <label className="label" htmlFor="cost-per-night">Cost Per Night</label>
+
+                            <CurrencyInput
+                                key="currency-input"
+                                placeholder="$0.00" 
+                                type="text"
+                                id="cost-per-night"
+                                name="cost-per-night"
+                                value={costPerNight}
+                                required
+                                className="financial-input"
+                                onChange={(event) => setCostPerNight(event.target.value)}
+                            />
+                            
+                        </div>
+
+                        <div className="tenant-input-div">
+                            <label className="label" htmlFor="last-name">Last Name</label>
+                            <input
+                                id="last-name"
+                                name="last-name"
+                                type="text"
+                                value={lastName}
+                                placeholder="last name"
+                                required
+                                className="tenant-input"
+                                onChange={(event) => setLastName(event.target.value)}
+                            />
+                        </div>
+
+                        <div className="tenant-input-div">
+                            <label className="label" htmlFor="phone">Phone</label>
+                            
+                            <input
+                                id="phone"
+                                name="phone"
+                                value={phone}
+                                placeholder="phone (optional)"
+                                className="tenant-input"
+                                onChange={(event) => setPhone(event.target.value)}
+                            />
+                        </div>
+
+                        <div className="tenant-input-div">
+                            <label className="label" htmlFor="email">Email</label>
+                            <input
+                                id="email"
+                                name="email"
+                                type="email"
+                                value={email}
+                                placeholder="email (optional)"
+                                className="tenant-input"
+                                onChange={(event) => setEmail(event.target.value)}
+                            />
+                        </div>
+                    </div> {/* end of tenant-container */}
 
 
                 </form>
