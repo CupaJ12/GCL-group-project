648ee7733b20b7909a1e80dfa83e422573396674
Khue Lee <khuelee55@gmail.com>
2023-03-30 18:03:10 -0500

    testing

--- database.sql
+++ database.sql
@@ -67,3 +67,6 @@ INSERT INTO "date" ("date") VALUES ('12-31-2023'); -- month/day/year
 INSERT INTO "money" ("money") VALUES (500);     -- no quotation marks
 INSERT INTO "money" ("money") VALUES ('300');	-- with quotation marks
 INSERT INTO "money" ("money") VALUES (1000.50);	-- with decimal
+
+
+-- testing
