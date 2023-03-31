4428a35303b8cf79671235d38e9f2c8880852c7e
Khue Lee <khuelee55@gmail.com>
2023-03-30 19:30:00 -0500

    added EditBoooking component

---
+++ src/components/EditBooking/EditBooking.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function EditBooking () {
+
+    return (
+        <div>
+            <h1>Edit Booking Page</h1>
+        </div>
+    )
+}
+
+export default EditBooking;
