849a9258ca10ffd98d039724f413a0c4daca9121
CupaJ12 <108900676+CupaJ12@users.noreply.github.com>
2023-04-02 03:24:07 -0500

    GET by ID route

--- server/routes/booking.router.js
+++ server/routes/booking.router.js
@@ -2,15 +2,27 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 
+// GET route to get single booking by ID
 
-
-
-
-
-
-
-
-
-
+router.get('/:id', (req, res) => {
+	if (req.isAuthenticated()) {
+		console.log('get booking by ID:', req.params.id);
+		const id = req.params.id;
+		const queryText = `
+	  SELECT * FROM "booking"
+	  WHERE "id" = $1;`;
+		pool
+			.query(queryText, [id])
+			.then((result) => {
+				res.send(result.rows);
+			})
+			.catch((error) => {
+				console.log('post route booking by ID error: ', error);
+				res.sendStatus(500);
+			});
+	} else {
+		res.sendStatus(403); // forbidden status code
+	}
+});
 
 module.exports = router;
