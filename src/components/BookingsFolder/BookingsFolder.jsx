39b636d54aa70a12566adb045229aa1983548b7a
Liam King <techy.willyk3@gmail.com>
2023-04-05 22:56:26 -0500

    Final bit of styling for the night.

--- src/components/BookingsFolder/BookingsFolder.jsx
+++ src/components/BookingsFolder/BookingsFolder.jsx
@@ -23,7 +23,7 @@ function BookingsFolder() {
     const handleDetails = (id) => {
         history.push(`/bookingdetails/${id}`);
     };
-// For bookings by search query
+    // For bookings by search query
     // const handleSearch = (event) => {
     //     event.preventDefault();
     //     dispatch({
@@ -34,25 +34,33 @@ function BookingsFolder() {
 
     return (
         <div className="booking-form-container">
-{/* Plan is to finish this GET bookings by search query as a group */}
-{/* insert on div line when ready to start working on it onSubmit={handleSearch} */}
-{/* insert on button line when ready to start working on it 'onChange={(event) => setSearch(event.target.value)} */}
+            {/* Plan is to finish this GET bookings by search query as a group */}
+            {/* insert on div line when ready to start working on it onSubmit={handleSearch} */}
+            {/* insert on button line when ready to start working on it 'onChange={(event) => setSearch(event.target.value)} */}
             <div>
                 <input className="tenant-input"></input>
                 <button className="submit-btn">
                     Search
                 </button>
             </div>
+
+            <h1 className="title" >
+                <u> View All Bookings: </u>
+            </h1>
+
+            <br>
+            </br>
+
             <section>
                 {bookings.map(booking => (
                     <div className="bookings" key={booking.id}>
+                        <h1>___________________________________________</h1>
                         <h3> Property Number: <i><u> {booking.property_id} </u></i> </h3>
                         <h3> Is Booking Finalized?: <i><u> {booking.finalized} </u></i> </h3>
                         <h3> Customer First Name: <i><u> {booking.customer_first_name} </u></i> </h3>
                         <h3> Customer Last Name: <i><u> {booking.customer_last_name} </u></i> </h3>
                         <h3> Checkin Date: <i><u> {booking.check_in_date} </u></i> </h3>
                         <h3> Checkout Date: <i><u> {booking.check_out_date} </u></i> </h3>
-                        <h3> Is Booking Finalized?: <i><u> {booking.finalized} </u></i> </h3>
                         {/* <h3> Customer Address: <i><u> {booking.customer_address} </u></i> </h3>
                             <h3> Customer Email: <i><u> {booking.customer_email} </u></i> </h3>
                             <h3> Vendor: <i><u> {booking.vendor} </u></i> </h3>
