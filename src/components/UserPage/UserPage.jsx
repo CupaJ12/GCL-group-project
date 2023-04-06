a551ae673614b892442bb87574a4f788a93324c7
Liam King <techy.willyk3@gmail.com>
2023-04-05 21:25:06 -0500

    Added two buttons to the user's home page called All Bookings and Add Bookings.

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import LogOutButton from '../LogOutButton/LogOutButton';
-import {useSelector} from 'react-redux';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 function UserPage() {
   // this component doesn't do much to start, just renders some user reducer info to the DOM
@@ -9,7 +10,27 @@ function UserPage() {
     <div className="container">
       <h2>Welcome, {user.username}!</h2>
       <p>Your ID is: {user.id}</p>
+
+      <Link to="/allbookings">
+        All Bookings
+      </Link>
+
+      <br>
+      </br>
+      <br>
+      </br>
+
+      <Link to="/bookingform">
+        Add Booking
+      </Link>
+
+      <br>
+      </br>
+      <br>
+      </br>
+
       <LogOutButton className="btn" />
+
     </div>
   );
 }
