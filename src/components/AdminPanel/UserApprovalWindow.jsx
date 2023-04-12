82dbc1e762981502a83c3ec207587824e204ce7d
Bryan Highhill <bryanhighhill@gmail.com>
2023-04-12 12:40:30 -0500

    created conditional on userPage to reflect if user is approved or not. added css and styling to keep consistent with booking form and overall theme. added registration date and date formatting  successfully to userapprovalwindow.jsx which is consumed in adminpanel

--- src/components/AdminPanel/UserApprovalWindow.jsx
+++ src/components/AdminPanel/UserApprovalWindow.jsx
@@ -1,14 +1,15 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { useEffect, useState } from 'react';
 import './AdminPanel.css';
-import CancelValidation from '../CancelValidationModal/CancelValidationModal';
+import ConfirmUserDenialModal from '../ConfirmUserDenialModal/ConfirmUserDenialModal';
 
 
 const UserApprovalWindow = () => { 
+    const [approvedUser, setApprovedUser] = useState('');
     const [cancelModalVisible, setCancelModalVisible] = useState(false);
     const dispatch = useDispatch();
+    const options = { year: 'numeric', month: 'long', day: 'numeric' };
     const unapprovedUsers = useSelector(store => store.unapprovedUsers);
-    const [approvedUser, setApprovedUser] = useState('');
 
     useEffect(() => {
         dispatch({ type: 'FETCH_UNAPPROVED_USERS' });
@@ -36,13 +37,15 @@ const UserApprovalWindow = () => {
             <table>
                 <tbody>
                     {unapprovedUsers.map((user, index) => {
+                        const registrationDate = new Date(user.registration_date);
+
                         return (
                             <tr>
-                                <td className="unapproved-user-column">{user.username}</td>
-                                <td className="unapproved-user-column">registration date here</td>
+                                <td className="unapproved-username-column">{user.username}</td>
+                                <td className="unapproved-user-column">registered on: <br /> {registrationDate.toLocaleDateString('en-US', options)}</td>
                                 <td className="unapproved-user-column"><button className="approve-btn" onClick={() => {approve(user.id), setApprovedUser(user.username)}}>Approve</button></td>
                                 <td className="unapproved-user-column"><button className="deny-btn" onClick={() => setCancelModalVisible(true)}>Deny</button></td> 
-                                <CancelValidation show={cancelModalVisible} onConfirm={() => deny(user.id)} onDeny={() => setCancelModalVisible(false)} />
+                                <ConfirmUserDenialModal show={cancelModalVisible} onConfirm={() => deny(user.id)} onDeny={() => setCancelModalVisible(false)} />
                             </tr>
                         )
                     })}
