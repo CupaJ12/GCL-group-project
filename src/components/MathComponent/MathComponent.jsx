7057cd37a99c79d697ff532eb1cf90d2c91d383b
Bryan Highhill <bryanhighhill@gmail.com>
2023-04-13 17:09:32 -0500

    added comments to explain the MathComponent, how it functions, and what props it needs to work correctly

--- src/components/MathComponent/MathComponent.jsx
+++ src/components/MathComponent/MathComponent.jsx
@@ -1,48 +1,40 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
+// Math Component needs two prop values to work - "booking" which needs to be the booking object from the DB, and "type" which needs to be one of two string values - 'gross' or 'net'
 const MathComponent = ({ booking, type }) => {
-    const [change, setChange] = useState(0);
     const [grossBookingAmount, setGrossBookingAmount] = useState(0);
     const [netPayout, setNetPayout] = useState(0);
     const [rentalCost, setRentalCost] = useState(0);
-    const checkIn = new Date(booking.check_in_date);
-    const checkOut = new Date(booking.check_out_date);
+    const checkIn = new Date(booking.check_in_date); // converts check in date from string to date
+    const checkOut = new Date(booking.check_out_date); // converts check out date from string to date
 
-    console.log('in math component with booking: ', booking);
-
-
-    //calculates rental cost & gross booking amount whenever booking value changes
+    //calculates rental cost amount whenever booking value changes
     useEffect(() => { 
         calculateRentalCost();
     }, [booking]);
 
-    useEffect(() => { 
-        calculateRentalCost();
-    }, [booking.check_in_date]);
-
+    // calculates gross whenever rentalCost value is set
     useEffect(() => { 
         calculateGross();
     }, [rentalCost]);
 
-    //function to figure out total rental cost - 
+    // function to figure out total rental cost - 
     const calculateRentalCost = () => {
         if (checkIn != null && checkOut != null && checkOut.getTime() > checkIn.getTime()) { // checks for null values and that checkout date is after checkin date
-        //.getTime() converts to date to milliseconds since jan 1 1970 at midnight, divide 1000 to get number of seconds, divide by 60 to get minutes, then 60 to get hours, then 24 to get days    
-        let numberOfNights = (((((checkOut.getTime() - checkIn.getTime()) / 1000) / 60 ) /60 ) / 24);
-        console.log('num of nights: ', numberOfNights);
-        console.log('per night cost: ', Number(booking.cost_per_night.replace(/[^0-9.]/g, '')) * numberOfNights);
-        setRentalCost((Number(booking.cost_per_night.replace(/[^0-9.]/g, ''))) * numberOfNights); //converts cost to number, removes any character that isnt integer or decimal, multiplies by number of nights
-    }; 
+
+            // .getTime() converts to date to milliseconds since jan 1 1970 at midnight, divide 1000 to get number of seconds, divide by 60 to get minutes, then 60 to get hours, then 24 to get days    
+            let numberOfNights = (((((checkOut.getTime() - checkIn.getTime()) / 1000) / 60 ) /60 ) / 24);
+
+            // converts cost to number, removes any character that isnt integer or decimal, multiplies by number of nights
+            setRentalCost((Number(booking.cost_per_night.replace(/[^0-9.]/g, ''))) * numberOfNights);
+        }; 
     };
 
     const calculateGross = () => {
         if (!booking.tax_responsible && rentalCost > 0) {  
             let grossBookingAmount = rentalCost + (Number(booking.pet_fee.replace(/[^0-9.]/g, ''))) + (Number(booking.cleaning_fee.replace(/[^0-9.]/g, ''))) + (Number(booking.lodging_tax.replace(/[^0-9.]/g, '')));
             let feesOwed = (Number(booking.vendor_commission.replace(/[^0-9.]/g, ''))) + (Number(booking.vendor_fee.replace(/[^0-9.]/g, ''))) + (Number(booking.discount.replace(/[^0-9.]/g, '')));
-            
-            console.log('gross: ', typeof grossBookingAmount);
-            console.log('fees owed: ', feesOwed);
             setGrossBookingAmount(grossBookingAmount);
             setNetPayout(grossBookingAmount - feesOwed);
         }
@@ -53,6 +45,8 @@ const MathComponent = ({ booking, type }) => {
             setNetPayout(grossBookingAmount - feesOwed);
         };    
     };
+
+    // conditional on return to show which math value to return - dependendant on "type" prop
     if (type === 'net') {
         return netPayout.toFixed(2);
     } else 
