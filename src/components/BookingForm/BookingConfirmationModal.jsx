ddfe24a5c41e777c0a9612edf82293ed82969388
Khue Lee <khuelee55@gmail.com>
2023-04-23 17:05:11 -0500

    fixed CSSTransition warning in BookingConfirmationModal

--- src/components/BookingForm/BookingConfirmationModal.jsx
+++ src/components/BookingForm/BookingConfirmationModal.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import { useHistory } from 'react-router-dom';
 import { CSSTransition } from "react-transition-group";
 import ReactDOM from "react-dom";
@@ -6,17 +6,19 @@ import '../CancelValidationModal/CancelValidationModal.css';
 
 const BookingConfirmationModal = ({ onConfirm, show }) => {
     const history = useHistory();
+    const elementRef = useRef(null);
 
     // ReactDOM.createPortal create the div outside of the parent so it won't break the parent's css
     return ReactDOM.createPortal(
         // CSSTransition is a built-in method that render or remove the div into the DOM
         // npm install react-transition-group, to use this built-in method
         <CSSTransition
+            nodeRef={elementRef}
             in={show}
             unmountOnExit
             timeout={{ enter: 0, exit: 300 }}
         >
-            <div className="modal">
+            <div className="modal" ref={elementRef}>
 
                 <div className="booking-confirmation-container">
                     <h1>New booking created!</h1>
