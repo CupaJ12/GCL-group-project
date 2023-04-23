bab88b9d8883103c13c969ee2712cc19d4bdf9b8
Khue Lee <khuelee55@gmail.com>
2023-04-23 16:45:39 -0500

    toggles are not set to thier right colors on load

--- src/components/EditFinancialModal/EditFinancialToggleSwitch.jsx
+++ src/components/EditFinancialModal/EditFinancialToggleSwitch.jsx
@@ -1,20 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './EditFinancialToggleSwitch.css';
 
 const TaxToggleSwitch = (props) => {
+    // console.log(props.taxResponsibility);
 
-    console.log(props.taxResponsibility);
+    const [toggleChecked, setToggleChecked] = useState(props.taxResponsibility);
 
     return (
         <label className="toggle">
             <div className="slider-div">
                 <label className="tax-responsibility-text">Tax Responsibility</label>
             </div>
-            <input 
-                disabled={props.finalized === true} 
-                className="slider-input" 
-                type="checkbox" 
+            <input
+                disabled={props.finalized === true}
+                className="slider-input"
+                type="checkbox"
+                checked={toggleChecked}
                 onClick={() => props.setTaxResponsibility()}
+                onChange={() => setToggleChecked(!toggleChecked)}
             />
             <section className="slider round"></section>
             <div className="responsibility-text">{props.taxResponsibility === false ? 'them' : 'us'}</div>
@@ -23,16 +26,23 @@ const TaxToggleSwitch = (props) => {
 };
 
 const FeesFinalizedToggleSwitch = (props) => {
+    
+    const [toggleChecked, setToggleChecked] = useState(props.finalized);
 
     return (
         <label className="toggle">
             <div className="slider-div">
                 <label className="tax-responsibility-text">Fees Finalized</label>
             </div>
-            <input className="slider-input" type="checkbox" onClick={() => props.setFinalized()}/>
-
+            <input 
+                className="slider-input" 
+                type="checkbox" 
+                checked={toggleChecked} 
+                onClick={() => props.setFinalized()} 
+                onChange={() => setToggleChecked(!toggleChecked)}
+            />
             <section className="slider round"></section>
-            <div className="responsibility-text">{props.finalized === false ? 'yes' : 'no'}</div>
+            <div className="responsibility-text">{props.finalized === false ? 'no' : 'yes'}</div>
         </label>
     )
 };
